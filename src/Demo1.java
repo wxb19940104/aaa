e64685c6b9d3b88170376adfcb82f31aa035baf6
wxb19940104 <923507532@qq.com>
2017-08-22 09:28:36 +0800

    方法

--- src/Demo1.java
+++ src/Demo1.java
@@ -4,6 +4,10 @@
 
 public class Demo1 {
     int x=9;
+    public int sum(int x,int y){
+        System.out.println(x+y);
+        return  x+y;
+    }
     public static void main(String[] args) {
         System.out.println("曹源");
 
